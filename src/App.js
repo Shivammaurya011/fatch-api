faf4bf0458127ecb1084e815b0e0c1b596bbc8f2
Shivammaurya011 <shivammaurya011@gmail.com>
2023-01-29 17:02:40 +0530

    commited

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FatchAPI from './FatchAPI';
+
+function App() {
+  return (
+    <div className="App">
+      <FatchAPI/>
+    </div>
+  );
+}
+
+export default App;
